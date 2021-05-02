e80a7b6a43a87d4402b345361c8f17617c0ac6eb
jpduong12 <jpduong12@gmail.com>
2021-05-02 16:17:54 -0700

    Lots of comments and implemented Card and Deck plus enum classes

---
+++ groupBlackjack/Card.cpp
@@ -0,0 +1,83 @@
+//***************************************************************************
+// File name:  Card.cpp
+// Author:     John Duong
+// Date:       5/7/2021
+// Class:      CS485
+// Assignment: Blackjack
+// Purpose:    Function implementations for Card class
+//***************************************************************************
+
+#include "Card.h"
+
+//***************************************************************************
+// Constructor:	Card
+//
+// Description: Initializes member variables
+//
+// Parameters:  eSuit						 - enum for suit of card
+//							eValue					 - enum for value of card
+//							mbIsGlobalFaceUp - whether the values of the card is shown
+//
+// Return:			None
+//***************************************************************************
+Card::Card (Suit eSuit, Value eValue)
+{
+	const bool FACE_DOWN = false;
+
+	meSuit = eSuit;
+	meValue = eValue;
+	mbIsGlobalFaceUp = FACE_DOWN;
+}
+//***************************************************************************
+// Function:		isFaceUp
+//
+// Description: Returns true if card is face up, false otherwise
+//
+// Parameters:  None
+//
+// Return:			True if face up, false otherwise
+//***************************************************************************
+bool Card::isFaceUp ()
+{
+	return mbIsGlobalFaceUp;
+}
+//***************************************************************************
+// Function:		flip
+//
+// Description: Toggle mbIsGlobalFaceUp
+//
+// Parameters:  None
+//
+// Return:			None
+//***************************************************************************
+void Card::flip ()
+{
+	mbIsGlobalFaceUp = !mbIsGlobalFaceUp;
+}
+//***************************************************************************
+// Function:		getSuit
+//
+// Description: returns suit of card
+//
+// Parameters:  None
+//
+// Return:			enum suit of card
+//***************************************************************************
+Suit Card::getSuit ()
+{
+	return meSuit;
+}
+//***************************************************************************
+// Function:		getValue
+//
+// Description: return value of card
+//
+// Parameters:  None
+//
+// Return:			enum value of card
+//***************************************************************************
+Value Card::getValue ()
+{
+	return meValue;
+}
+
--- groupBlackjack/Value.h
+++ groupBlackjack/Value.h
@@ -1,1 +1,15 @@
+//***************************************************************************
+// File name:  
+// Author:     
+// Date:       5/7/2021
+// Class:      CS485
+// Assignment: Blackjack
+// Purpose:    
+//***************************************************************************
 #pragma once
+
+enum class Value
+{
+	Ace, Two, Three, Four, Five, Six, Seven,
+	Eight, Nine, Ten, Jack, Queen, King, Count = 13
+};
