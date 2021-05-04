0d6244124e13b80b29ca97af11846b5a18061456
Julian <bunc8372@pacificu.edu>
2021-05-03 22:07:59 -0700

    Added BlackjackTextUI to main project

---
+++ groupBlackjack/BlackjackTextUI.cpp
@@ -0,0 +1,58 @@
+#include "BlackjackTextUI.h"
+
+
+
+void BlackjackTextUI::newGame(int)
+{
+
+}
+
+void BlackjackTextUI::nextRound()
+{
+	
+}
+
+void BlackjackTextUI::stand()
+{
+
+}
+
+void BlackjackTextUI::drawCard()
+{
+
+}
+
+void BlackjackTextUI::split()
+{
+
+}
+
+void BlackjackTextUI::bet(long long amount)
+{
+
+}
+
+void BlackjackTextUI::onStand()
+{
+
+}
+
+void BlackjackTextUI::onDrawCard()
+{
+
+}
+
+void BlackjackTextUI::onSplit()
+{
+
+}
+
+void BlackjackTextUI::onBet(long long amount)
+{
+
+}
+
+void BlackjackTextUI::render()
+{
+
+}
