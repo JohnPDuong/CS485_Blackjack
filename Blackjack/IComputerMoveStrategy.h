cfb665c49e492445c56840f918f1ecd1d30aa0fd
jpduong12 <jpduong12@gmail.com>
2021-05-04 21:30:22 -0700

    asdf

--- Blackjack/IComputerMoveStrategy.h
+++ Blackjack/IComputerMoveStrategy.h
@@ -11,12 +11,11 @@
 
 #include "IMoveStrategy.h"
 
-class IComputerMoveStrategy : public IMoveStrategy 
-{
-  public:
-    virtual bool determineMove(Hand& cCurrentHand,
+class IComputerMoveStrategy : public IMoveStrategy {
+public:
+  virtual bool determineMove(Hand& cCurrentHand,
                              std::shared_ptr<IMove> pcCurrentMove,
                              std::vector<Card>& cTableCards) = 0;
-    virtual bool determineBet(Player& player, Money& bet) = 0;
-    virtual bool isHuman() = 0;
+  virtual bool determineBet(Player& player, Money& bet) = 0;
+  virtual bool isHuman() = 0;
 };
