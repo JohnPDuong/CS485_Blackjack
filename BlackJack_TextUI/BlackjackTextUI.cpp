b5e393120d11eb7ef15e46ff4ea9f41b1dbf7f5d
Julian <bunc8372@pacificu.edu>
2021-05-06 10:43:17 -0700

    doing more blackjack textUI stuff

--- BlackJack_TextUI/BlackjackTextUI.cpp
+++ BlackJack_TextUI/BlackjackTextUI.cpp
@@ -133,15 +133,15 @@ void BlackjackTextUI::onGameStartup()
 
 void BlackjackTextUI::playGame()
 {
-	const char BET = '0';
-	const char STAND = '1';
-	const char DRAW = '2';
-	const char SPLIT = '3';
+	const int BET = 0;
+	const int STAND = 1;
+	const int DRAW = 2;
+	const int SPLIT = 3;
 
 	bool bKeepPlaying = true;
 	long long betAmount = -1;
 	int turns = 0;
-	char move = '-';
+	int move;
 
 	while (bKeepPlaying)
 	{
@@ -159,13 +159,43 @@ void BlackjackTextUI::playGame()
 		// Prints the move options if the human's turn is still going
 		while (move != STAND && mpcPresenter->result() != Status::Bust)
 		{
+			move = SPLIT;
 			std::cout << "OPTIONS: \n(0) Bet \n(1) Stand \n(2) Draw\n";
 			if (mpcPresenter->canSplit())
 			{
 				std::cout << "(3) Split\n";
 			}
 			std::cout << "Enter your move: ";
-			std::cin >> move;
+			do
+			{
+				std::cin >> move;
+			} while ((move == SPLIT && !mpcPresenter->canSplit()) || (move > SPLIT || move < BET));
+
+			// Executes selected move
+			switch (move)
+			{
+			case BET:
+				std::cout << "Bet amount: ";
+				do
+				{
+					std::cin >> betAmount;
+				} while (betAmount < 0 || betAmount > mpcPresenter->getBalance());
+				mpcPresenter->bet(betAmount);
+				break;
+
+			case STAND:
+				mpcPresenter->stand();
+				break;
+
+			case DRAW:
+				mpcPresenter->draw();
+				break;
+
+			case SPLIT:
+				mpcPresenter->split();
+				break;
+			}
+
 		}
 
 
