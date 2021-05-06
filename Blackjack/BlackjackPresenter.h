ad4a3fcad352a43d2aafe9b5a01d18142618d3cc
Julian <bunc8372@pacificu.edu>
2021-05-05 22:26:16 -0700

    j

--- Blackjack/BlackjackPresenter.h
+++ Blackjack/BlackjackPresenter.h
@@ -22,23 +22,24 @@ class BlackjackPresenter : public IBlackjackPresenter
 
 		virtual void newGame(int players);
 
-	virtual void nextRound();
-	virtual void endGame();
-	virtual Status result();
-	//virtual void addBalance(long long amount) = 0;
-	virtual long long getBalance();
-	virtual std::vector<std::string> getCurrentPlayerHand();
-	virtual int getNumPlayers();
-	virtual std::string getName();
-	virtual void setName(std::string name);
+		virtual void nextRound();
+		virtual void endGame();
+		virtual Status result();
+		//virtual void addBalance(long long amount) = 0;
+		virtual long long getBalance();
+		virtual std::vector<std::string> getCurrentPlayerHand();
+		virtual int getNumPlayers();
+		virtual std::string getName();
+		virtual void setName(std::string name);
 
 		virtual void stand();
 		virtual void bet(long long amount);
 		virtual void split();
 		virtual void draw();
 
-	virtual void makeMove();
-	virtual void updateView();
+		virtual void makeMove();
+		virtual bool isHuman();
+		virtual void updateView();
 
 	private:
 		BlackjackModel mcModel;
