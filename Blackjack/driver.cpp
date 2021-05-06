ba931c2b564305f9bef9aa7862dda49a5b5ab777
Liannium <meliaerinelizabeth@gmail.com>
2021-05-06 12:55:37 -0700

    fixed some stuff

--- Blackjack/driver.cpp
+++
@@ -1,7 +0,0 @@
-//This is just so it will actually build (otherwise the project gives an unresolved external linker error because nomain.
-
-#include "BlackjackModel.h"
-int main() {
-	std::cout << "Why are you running this this is just to get it to load.";
-	return 0;
-}
