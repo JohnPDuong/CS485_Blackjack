affd0074ec687980ad6924a5b36a52aa1e304db8
Kitt Oster <isaaceoster@gmail.com>
2021-05-07 02:56:49 -0700

    marking everything override

--- Blackjack/BlackjackPresenterSDL.h
+++ Blackjack/BlackjackPresenterSDL.h
@@ -20,50 +20,50 @@ public:
 	BlackjackPresenterSDL (IBlackjackView* pcView, int numPlayers, int numDecks);
 	virtual ~BlackjackPresenterSDL ();
 
-	virtual bool newGame (int players);
-	virtual bool newGame ();
+	virtual bool newGame (int players) override;
+	virtual bool newGame () override;
 
-	virtual void nextRound ();
-	virtual void endGame ();
-	virtual std::vector<Status> endRound();
-	virtual bool roundOngoing();
-	virtual Status result ();
+	virtual void nextRound () override;
+	virtual void endGame () override;
+	virtual std::vector<Status> endRound() override;
+	virtual bool roundOngoing() override;
+	virtual Status result () override;
 	//virtual void addBalance(long long amount) = 0;
-	virtual long long getBalance ();
-	virtual std::vector<std::string> getCurrentPlayerHand ();
-	virtual std::vector<std::vector<std::string>> getAllCards ();
-	virtual std::vector<std::string> getDealerCards ();
-	virtual std::vector<std::string> getAllCurrentPlayerHands();
-	virtual int getNumPlayers ();
-	virtual std::string getName ();
+	virtual long long getBalance () override;
+	virtual std::vector<std::string> getCurrentPlayerHand () override;
+	virtual std::vector<std::vector<std::string>> getAllCards () override;
+	virtual std::vector<std::string> getDealerCards () override;
+	virtual std::vector<std::string> getAllCurrentPlayerHands() override;
+	virtual int getNumPlayers () override;
+	virtual std::string getName () override;
 	virtual void setName (std::string name, int index) override;
-	virtual void setPlayerType (std::string stratType, int index);
-	virtual std::vector<std::string> listMoveStratTypes();
+	virtual void setPlayerType (std::string stratType, int index) override;
+	virtual std::vector<std::string> listMoveStratTypes() override;
 
-	virtual std::string getName(int index);
-	virtual long long getBalance(int index);
+	virtual std::string getName(int index) override;
+	virtual long long getBalance(int index) override;
 
-	virtual void setBet(long long amount, int index);
-	virtual long long getBet(int index);
+	virtual void setBet(long long amount, int index) override;
+	virtual long long getBet(int index) override;
 
-	virtual void stand ();
-	virtual bool bet (long long amount);
-	virtual void setBalance(long long amount, int index);
+	virtual void stand () override;
+	virtual bool bet (long long amount) override;
+	virtual void setBalance(long long amount, int index) override;
 
-	virtual void split ();
-	virtual void draw ();
+	virtual void split () override;
+	virtual void draw () override;
 
-	virtual void makeMove ();
-	virtual bool isHuman ();
-	virtual bool isHuman(int index);
-	virtual bool canSplit ();
-	virtual bool isBetTime();
+	virtual void makeMove () override;
+	virtual bool isHuman () override;
+	virtual bool isHuman(int index) override;
+	virtual bool canSplit () override;
+	virtual bool isBetTime() override;
 
-	virtual void updateView ();
-	virtual void doCPUMoves();
-	virtual void doCPUBets();
+	virtual void updateView () override;
+	virtual void doCPUMoves() override;
+	virtual void doCPUBets() override;
 
-	virtual int getCurrentPlayer();
+	virtual int getCurrentPlayer() override;
 
 private:
 	BlackjackModel mcModel;
