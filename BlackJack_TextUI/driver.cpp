be027c097fde4ab78f3a492f750a5f65b24665e9
Julian <bunc8372@pacificu.edu>
2021-05-07 03:33:25 -0700

    vgbhj

--- BlackJack_TextUI/driver.cpp
+++ BlackJack_TextUI/driver.cpp
@@ -1,3 +1,12 @@
+//***************************************************************************
+// File name:  main.cpp
+// Author:     John, Julian, Kitt, Erin
+// Date:       5/7/2021
+// Class:      CS485
+// Assignment: Blackjack
+// Purpose:    Runs the TextUI version of blackjack
+//***************************************************************************
+
 #include <iostream>
 #include "BlackjackTextUI.h"
 #include "../Blackjack/BlackjackModel.h"
@@ -6,29 +15,12 @@ using namespace std;
 
 int main()
 {	
-	/*
-	BlackjackModel cModel;
-
-	cModel.newGame(3); for (int i = 0; i < 3 - 1; i++)
-	{
-		cModel.setPlayerType("Card Counter", i);
-	}
-	cModel.setPlayerType("Human", 3 - 1);
-	cout << "cool";
-
-	while (!cModel.isHuman())
-	{
-		cModel.makeMove();
-	}
-	cout << "moces done";*/
-
 	BlackjackTextUI cGame;
 	
 	system("cls");
 	cGame.printWelcome();
 	cGame.onGameStartup();
 	cGame.playGame();
-
 
 	return EXIT_SUCCESS;
 }
